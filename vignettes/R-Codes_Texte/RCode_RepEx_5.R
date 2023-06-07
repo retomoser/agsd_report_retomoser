fd06001d47e2b3aafdb717d9838514f04ed909ab
Reto Moser <redimoser@bluewin.ch>
2023-06-07 22:20:10 +0200

    shorten RCode

--- vignettes/R-Codes_Texte/RCode_RepEx_5.R
+++ vignettes/R-Codes_Texte/RCode_RepEx_5.R
@@ -45,6 +45,55 @@ mod_knn <- caret::train(
   metric = "RMSE"
 )
 
+# Fit KNN model k= 8
+mod_knn_8 <- caret::train(
+  pp, 
+  data = daily_fluxes_train |> drop_na(), 
+  method = "knn",
+  trControl = caret::trainControl(method = "none"),
+  tuneGrid = data.frame(k = 8),
+  metric = "RMSE"
+)
+
+# Fit KNN = 4
+mod_knn_4 <- caret::train(
+  pp, 
+  data = daily_fluxes_train |> drop_na(), 
+  method = "knn",
+  trControl = caret::trainControl(method = "none"),
+  tuneGrid = data.frame(k = 4),
+  metric = "RMSE"
+)
+
+# Fit KNN model k = 2
+mod_knn_2 <- caret::train(
+  pp, 
+  data = daily_fluxes_train |> drop_na(), 
+  method = "knn",
+  trControl = caret::trainControl(method = "none"),
+  tuneGrid = data.frame(k = 2),
+  metric = "RMSE"
+)
+
+# Fit KNN model k = 1
+mod_knn_1 <- caret::train(
+  pp, 
+  data = daily_fluxes_train |> drop_na(), 
+  method = "knn",
+  trControl = caret::trainControl(method = "none"),
+  tuneGrid = data.frame(k = 1),
+  metric = "RMSE"
+)
+
+# Fit KNN model k=100
+mod_knn_100 <- caret::train(
+  pp, 
+  data = daily_fluxes_train |> drop_na(), 
+  method = "knn",
+  trControl = caret::trainControl(method = "none"),
+  tuneGrid = data.frame(k = 100),
+  metric = "RMSE"
+)
 
 # make model evaluation into a function to reuse code
 eval_model <- function(mod, df_train, df_test){
@@ -111,378 +160,17 @@ eval_model(mod = mod_lm, df_train = daily_fluxes_train, df_test = daily_fluxes_t
 # KNN
 eval_model(mod = mod_knn, df_train = daily_fluxes_train, df_test = daily_fluxes_test)
 
-# Fit KNN model k= 8
-mod_knn_8 <- caret::train(
-  pp, 
-  data = daily_fluxes_train |> drop_na(), 
-  method = "knn",
-  trControl = caret::trainControl(method = "none"),
-  tuneGrid = data.frame(k = 8),
-  metric = "RMSE"
-)
-
-
-# make model evaluation into a function to reuse code
-eval_model <- function(mod, df_train, df_test){
-  
-  # add predictions to the data frames
-  df_train <- df_train |> 
-    drop_na()
-  df_train$fitted <- predict(mod, newdata = df_train)
-  
-  df_test <- df_test |> 
-    drop_na()
-  df_test$fitted <- predict(mod, newdata = df_test)
-  
-  # get metrics tables
-  metrics_train <- df_train |> 
-    yardstick::metrics(GPP_NT_VUT_REF, fitted)
-  
-  metrics_test <- df_test |> 
-    yardstick::metrics(GPP_NT_VUT_REF, fitted)
-  
-  # extract values from metrics tables
-  rmse_train <- metrics_train |> 
-    filter(.metric == "rmse") |> 
-    pull(.estimate)
-  rsq_train <- metrics_train |> 
-    filter(.metric == "rsq") |> 
-    pull(.estimate)
-  
-  rmse_test <- metrics_test |> 
-    filter(.metric == "rmse") |> 
-    pull(.estimate)
-  rsq_test <- metrics_test |> 
-    filter(.metric == "rsq") |> 
-    pull(.estimate)
-  
-  # visualise as a scatterplot
-  # adding information of metrics as sub-titles
-  plot_1 <- ggplot(data = df_train, aes(GPP_NT_VUT_REF, fitted)) +
-    geom_point(alpha = 0.3) +
-    geom_smooth(method = "lm", se = FALSE, color = "red") +
-    geom_abline(slope = 1, intercept = 0, linetype = "dotted") +
-    labs(subtitle = bquote( italic(R)^2 == .(format(rsq_train, digits = 2)) ~~
-                              RMSE == .(format(rmse_train, digits = 3))),
-         title = "Training set") +
-    theme_classic()
-  
-  plot_2 <- ggplot(data = df_test, aes(GPP_NT_VUT_REF, fitted)) +
-    geom_point(alpha = 0.3) +
-    geom_smooth(method = "lm", se = FALSE, color = "red") +
-    geom_abline(slope = 1, intercept = 0, linetype = "dotted") +
-    labs(subtitle = bquote( italic(R)^2 == .(format(rsq_test, digits = 2)) ~~
-                              RMSE == .(format(rmse_test, digits = 3))),
-         title = "Test set") +
-    theme_classic()
-  
-  out <- cowplot::plot_grid(plot_1, plot_2)
-  
-  return(out)
-}
-
-
-
 # KNN =8
 eval_model(mod = mod_knn_8, df_train = daily_fluxes_train, df_test = daily_fluxes_test)
 
-# Fit KNN = 4
-mod_knn_4 <- caret::train(
-  pp, 
-  data = daily_fluxes_train |> drop_na(), 
-  method = "knn",
-  trControl = caret::trainControl(method = "none"),
-  tuneGrid = data.frame(k = 4),
-  metric = "RMSE"
-)
-
-
-# make model evaluation into a function to reuse code
-eval_model <- function(mod, df_train, df_test){
-  
-  # add predictions to the data frames
-  df_train <- df_train |> 
-    drop_na()
-  df_train$fitted <- predict(mod, newdata = df_train)
-  
-  df_test <- df_test |> 
-    drop_na()
-  df_test$fitted <- predict(mod, newdata = df_test)
-  
-  # get metrics tables
-  metrics_train <- df_train |> 
-    yardstick::metrics(GPP_NT_VUT_REF, fitted)
-  
-  metrics_test <- df_test |> 
-    yardstick::metrics(GPP_NT_VUT_REF, fitted)
-  
-  # extract values from metrics tables
-  rmse_train <- metrics_train |> 
-    filter(.metric == "rmse") |> 
-    pull(.estimate)
-  rsq_train <- metrics_train |> 
-    filter(.metric == "rsq") |> 
-    pull(.estimate)
-  
-  rmse_test <- metrics_test |> 
-    filter(.metric == "rmse") |> 
-    pull(.estimate)
-  rsq_test <- metrics_test |> 
-    filter(.metric == "rsq") |> 
-    pull(.estimate)
-  
-  # visualise as a scatterplot
-  # adding information of metrics as sub-titles
-  plot_1 <- ggplot(data = df_train, aes(GPP_NT_VUT_REF, fitted)) +
-    geom_point(alpha = 0.3) +
-    geom_smooth(method = "lm", se = FALSE, color = "red") +
-    geom_abline(slope = 1, intercept = 0, linetype = "dotted") +
-    labs(subtitle = bquote( italic(R)^2 == .(format(rsq_train, digits = 2)) ~~
-                              RMSE == .(format(rmse_train, digits = 3))),
-         title = "Training set") +
-    theme_classic()
-  
-  plot_2 <- ggplot(data = df_test, aes(GPP_NT_VUT_REF, fitted)) +
-    geom_point(alpha = 0.3) +
-    geom_smooth(method = "lm", se = FALSE, color = "red") +
-    geom_abline(slope = 1, intercept = 0, linetype = "dotted") +
-    labs(subtitle = bquote( italic(R)^2 == .(format(rsq_test, digits = 2)) ~~
-                              RMSE == .(format(rmse_test, digits = 3))),
-         title = "Test set") +
-    theme_classic()
-  
-  out <- cowplot::plot_grid(plot_1, plot_2)
-  
-  return(out)
-}
-
-
-# KNN
+# KNN 4
 eval_model(mod = mod_knn_4, df_train = daily_fluxes_train, df_test = daily_fluxes_test)
 
-
-
-
-# Fit KNN model k = 2
-mod_knn_2 <- caret::train(
-  pp, 
-  data = daily_fluxes_train |> drop_na(), 
-  method = "knn",
-  trControl = caret::trainControl(method = "none"),
-  tuneGrid = data.frame(k = 2),
-  metric = "RMSE"
-)
-
-
-# make model evaluation into a function to reuse code
-eval_model <- function(mod, df_train, df_test){
-  
-  # add predictions to the data frames
-  df_train <- df_train |> 
-    drop_na()
-  df_train$fitted <- predict(mod, newdata = df_train)
-  
-  df_test <- df_test |> 
-    drop_na()
-  df_test$fitted <- predict(mod, newdata = df_test)
-  
-  # get metrics tables
-  metrics_train <- df_train |> 
-    yardstick::metrics(GPP_NT_VUT_REF, fitted)
-  
-  metrics_test <- df_test |> 
-    yardstick::metrics(GPP_NT_VUT_REF, fitted)
-  
-  # extract values from metrics tables
-  rmse_train <- metrics_train |> 
-    filter(.metric == "rmse") |> 
-    pull(.estimate)
-  rsq_train <- metrics_train |> 
-    filter(.metric == "rsq") |> 
-    pull(.estimate)
-  
-  rmse_test <- metrics_test |> 
-    filter(.metric == "rmse") |> 
-    pull(.estimate)
-  rsq_test <- metrics_test |> 
-    filter(.metric == "rsq") |> 
-    pull(.estimate)
-  
-  # visualise as a scatterplot
-  # adding information of metrics as sub-titles
-  plot_1 <- ggplot(data = df_train, aes(GPP_NT_VUT_REF, fitted)) +
-    geom_point(alpha = 0.3) +
-    geom_smooth(method = "lm", se = FALSE, color = "red") +
-    geom_abline(slope = 1, intercept = 0, linetype = "dotted") +
-    labs(subtitle = bquote( italic(R)^2 == .(format(rsq_train, digits = 2)) ~~
-                              RMSE == .(format(rmse_train, digits = 3))),
-         title = "Training set") +
-    theme_classic()
-  
-  plot_2 <- ggplot(data = df_test, aes(GPP_NT_VUT_REF, fitted)) +
-    geom_point(alpha = 0.3) +
-    geom_smooth(method = "lm", se = FALSE, color = "red") +
-    geom_abline(slope = 1, intercept = 0, linetype = "dotted") +
-    labs(subtitle = bquote( italic(R)^2 == .(format(rsq_test, digits = 2)) ~~
-                              RMSE == .(format(rmse_test, digits = 3))),
-         title = "Test set") +
-    theme_classic()
-  
-  out <- cowplot::plot_grid(plot_1, plot_2)
-  
-  return(out)
-}
-
-
-# KNN
+# KNN 2
 eval_model(mod = mod_knn_2, df_train = daily_fluxes_train, df_test = daily_fluxes_test)
 
-# Fit KNN model k = 1
-mod_knn_1 <- caret::train(
-  pp, 
-  data = daily_fluxes_train |> drop_na(), 
-  method = "knn",
-  trControl = caret::trainControl(method = "none"),
-  tuneGrid = data.frame(k = 1),
-  metric = "RMSE"
-)
-
-
-# make model evaluation into a function to reuse code
-eval_model <- function(mod, df_train, df_test){
-  
-  # add predictions to the data frames
-  df_train <- df_train |> 
-    drop_na()
-  df_train$fitted <- predict(mod, newdata = df_train)
-  
-  df_test <- df_test |> 
-    drop_na()
-  df_test$fitted <- predict(mod, newdata = df_test)
-  
-  # get metrics tables
-  metrics_train <- df_train |> 
-    yardstick::metrics(GPP_NT_VUT_REF, fitted)
-  
-  metrics_test <- df_test |> 
-    yardstick::metrics(GPP_NT_VUT_REF, fitted)
-  
-  # extract values from metrics tables
-  rmse_train <- metrics_train |> 
-    filter(.metric == "rmse") |> 
-    pull(.estimate)
-  rsq_train <- metrics_train |> 
-    filter(.metric == "rsq") |> 
-    pull(.estimate)
-  
-  rmse_test <- metrics_test |> 
-    filter(.metric == "rmse") |> 
-    pull(.estimate)
-  rsq_test <- metrics_test |> 
-    filter(.metric == "rsq") |> 
-    pull(.estimate)
-  
-  # visualise as a scatterplot
-  # adding information of metrics as sub-titles
-  plot_1 <- ggplot(data = df_train, aes(GPP_NT_VUT_REF, fitted)) +
-    geom_point(alpha = 0.3) +
-    geom_smooth(method = "lm", se = FALSE, color = "red") +
-    geom_abline(slope = 1, intercept = 0, linetype = "dotted") +
-    labs(subtitle = bquote( italic(R)^2 == .(format(rsq_train, digits = 2)) ~~
-                              RMSE == .(format(rmse_train, digits = 3))),
-         title = "Training set") +
-    theme_classic()
-  
-  plot_2 <- ggplot(data = df_test, aes(GPP_NT_VUT_REF, fitted)) +
-    geom_point(alpha = 0.3) +
-    geom_smooth(method = "lm", se = FALSE, color = "red") +
-    geom_abline(slope = 1, intercept = 0, linetype = "dotted") +
-    labs(subtitle = bquote( italic(R)^2 == .(format(rsq_test, digits = 2)) ~~
-                              RMSE == .(format(rmse_test, digits = 3))),
-         title = "Test set") +
-    theme_classic()
-  
-  out <- cowplot::plot_grid(plot_1, plot_2)
-  
-  return(out)
-}
-
-
-# KNN
+# KNN 1
 eval_model(mod = mod_knn_1, df_train = daily_fluxes_train, df_test = daily_fluxes_test)
-
-# Fit KNN model k=100
-mod_knn_100 <- caret::train(
-  pp, 
-  data = daily_fluxes_train |> drop_na(), 
-  method = "knn",
-  trControl = caret::trainControl(method = "none"),
-  tuneGrid = data.frame(k = 100),
-  metric = "RMSE"
-)
-
-
-# make model evaluation into a function to reuse code
-eval_model <- function(mod, df_train, df_test){
-  
-  # add predictions to the data frames
-  df_train <- df_train |> 
-    drop_na()
-  df_train$fitted <- predict(mod, newdata = df_train)
-  
-  df_test <- df_test |> 
-    drop_na()
-  df_test$fitted <- predict(mod, newdata = df_test)
-  
-  # get metrics tables
-  metrics_train <- df_train |> 
-    yardstick::metrics(GPP_NT_VUT_REF, fitted)
-  
-  metrics_test <- df_test |> 
-    yardstick::metrics(GPP_NT_VUT_REF, fitted)
-  
-  # extract values from metrics tables
-  rmse_train <- metrics_train |> 
-    filter(.metric == "rmse") |> 
-    pull(.estimate)
-  rsq_train <- metrics_train |> 
-    filter(.metric == "rsq") |> 
-    pull(.estimate)
-  
-  rmse_test <- metrics_test |> 
-    filter(.metric == "rmse") |> 
-    pull(.estimate)
-  rsq_test <- metrics_test |> 
-    filter(.metric == "rsq") |> 
-    pull(.estimate)
-  
-  # visualise as a scatterplot
-  # adding information of metrics as sub-titles
-  plot_1 <- ggplot(data = df_train, aes(GPP_NT_VUT_REF, fitted)) +
-    geom_point(alpha = 0.3) +
-    geom_smooth(method = "lm", se = FALSE, color = "red") +
-    geom_abline(slope = 1, intercept = 0, linetype = "dotted") +
-    labs(subtitle = bquote( italic(R)^2 == .(format(rsq_train, digits = 2)) ~~
-                              RMSE == .(format(rmse_train, digits = 3))),
-         title = "Training set") +
-    theme_classic()
-  
-  plot_2 <- ggplot(data = df_test, aes(GPP_NT_VUT_REF, fitted)) +
-    geom_point(alpha = 0.3) +
-    geom_smooth(method = "lm", se = FALSE, color = "red") +
-    geom_abline(slope = 1, intercept = 0, linetype = "dotted") +
-    labs(subtitle = bquote( italic(R)^2 == .(format(rsq_test, digits = 2)) ~~
-                              RMSE == .(format(rmse_test, digits = 3))),
-         title = "Test set") +
-    theme_classic()
-  
-  out <- cowplot::plot_grid(plot_1, plot_2)
-  
-  return(out)
-}
-
 
 # KNN = 100
 eval_model(mod = mod_knn_100, df_train = daily_fluxes_train, df_test = daily_fluxes_test)
-
-
